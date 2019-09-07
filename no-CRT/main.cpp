6d4fe1aef9b298c2425aebe8eec6cab07b159080
Daniel Krupiński <danielkrupinski@outlook.com>
2019-09-07 14:04:56 +0200

    Add WinMain() function

---
+++ no-CRT/main.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    if (AllocConsole()) {
+        constexpr wchar_t message[]{ L"Hello world! but without CRT :)" };
+        WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), message, sizeof(message) / sizeof(wchar_t) - 1, nullptr, nullptr);
+
+        auto input{ L'\0' };
+
+        do {
+            DWORD charsRead;
+            ReadConsoleW(GetStdHandle(STD_INPUT_HANDLE), &input, 1, &charsRead, nullptr);
+        } while (!input);
+
+        FreeConsole();
+    }
+}
